3969ff4c52b2088e549933a818473586738e53b6
Gamaliel Padillo <gamalielgad1234@gmail.com>
2024-08-19 21:43:02 -0700

    devop: add fetch, clear readme

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
 import { Component } from "react";
 
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-        },
-        {
-          name: "Dracula",
-        },
-        {
-          name: "Zombie",
-        },
-      ],
+      monsters: [],
     };
   }
 
-  changeName() {
-    // is happening asynchronously thats why even if we console.log(this.state)
-    // after this line, it will still show the old state
-    //
-    // react batches these state updates so it can calculate the
-    // minimum number of changes to the DOM
-    // this.setState({ name: { firstNanme: "Yel", lastName: "Padillo" } });
-
-    // to fix this, we can pass a callback function to this.setState
-    // first function is the state update
-    // second function is the callback function
-    this.setState(
-      () => {
-        return { name: { firstNanme: "Yel", lastName: "Padillo" } };
-      },
-      () => {
-        // this function only fires once **ALL** update is done
-        console.log(this.state);
-      }
-    );
+  // lifecycle method
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
+
+  // changeName() {
+  //   // is happening asynchronously thats why even if we console.log(this.state)
+  //   // after this line, it will still show the old state
+  //   //
+  //   // react batches these state updates so it can calculate the
+  //   // minimum number of changes to the DOM
+  //   // this.setState({ name: { firstNanme: "Yel", lastName: "Padillo" } });
+
+  //   // to fix this, we can pass a callback function to this.setState
+  //   // first function is the state update
+  //   // second function is the callback function
+  //   this.setState(
+  //     () => {
+  //       return { name: { firstNanme: "Yel", lastName: "Padillo" } };
+  //     },
+  //     () => {
+  //       // this function only fires once **ALL** update is done
+  //       console.log(this.state);
+  //     }
+  //   );
+  // }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster, idx) => (
-          <h1 key={idx}>{monster.name}</h1>
-        ))}
+        {this.state.monsters.length > 0 ? (
+          this.state.monsters.map((monster, idx) => (
+            <div key={idx + monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          ))
+        ) : (
+          <h1>Loading...</h1>
+        )}
       </div>
     );
   }
