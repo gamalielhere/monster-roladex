12412395018e884162b5b48f07cc8a19e3ecff1f
Gamaliel Padillo <gamalielgad1234@gmail.com>
2024-08-19 21:46:57 -0700

    devop: stopping point

--- src/App.js
+++ src/App.js
@@ -14,7 +14,16 @@ class App extends Component {
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }));
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   // changeName() {
