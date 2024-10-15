5f07d1a1a1e308851b1a12e72a6f5baf8b4499d9
Gamaliel Padillo <gamalielgad1234@gmail.com>
2024-10-14 22:53:45 -0700

    feat: finish styles from course

--- src/App.js
+++ src/App.js
@@ -67,6 +67,7 @@ class App extends Component {
     });
     return (
       <div className="App">
+        <h1 className="app-title">Monster Rolodex</h1>
         <SearchInputComponent onSearchChange={onSearchChange} />
         <CardListComponent monsters={filtered} />
       </div>
